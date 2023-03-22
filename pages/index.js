373431a9b973ffc5660e37842683a88ac549eba3
ansharora28 <ansharorake2@gmail.com>
2023-03-22 17:55:46 +0530

    tailwind styling

--- pages/index.js
+++ pages/index.js
@@ -20,10 +20,13 @@ const Index = () => {
   }, []);
 
   return (
-    <div className="content">
-      <h1>Random Text Generator</h1>
-      <button onClick={generateText} disabled={isGenerating}>Generate Text</button>
-      <p>{text}</p>
+    <div className="container mx-auto md:px-20">
+      <h1 className="font-bold text-4xl pb-12 text-center">Random Text Generator</h1>
+      <p className=" font-bold text-3xl text-blue-400 text-center bg-green-200 w-25 mx-10">This is a NextJs + TailwindCSS project</p>
+      <div className="flex justify-center py-10">
+        <button className="rounded-lg text-3xl px-3 py-3 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..." onClick={generateText} disabled={isGenerating}>Generate Text</button>
+      </div>
+      <p className="">{text}</p>
     </div>
   );
 }
